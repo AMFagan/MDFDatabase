4850082c1cfb8e07024c9c3a1e8bd1ff5947c232
AndyF1996 <andrewfagan1996@gmail.com>
2019-07-05 11:44:30 +0100

    Created SQL server with basic class layout on it, as well as POC test script for the requirements tree.

---
+++ source/db_test.py
@@ -0,0 +1,27 @@
+#requires database to be already active
+
+import mysql.connector as con
+
+
+sql = con.connect(host='localhost',
+                  user='root',
+                  password='dapamaka',
+                  database='mdfdatabase')
+
+cursor = sql.cursor()
+
+cursor.execute("SELECT this_class_id, required_class_id FROM pre_requisites")
+
+output = cursor.fetchall()
+
+for line in output:
+    id0, id1 = line
+    q = "SELECT class_code FROM classes WHERE class_id = %s"
+
+    cursor.execute(q, (id0,))
+    this = cursor.fetchall()
+    cursor.execute(q, (id1,))
+    req = cursor.fetchall()
+
+    print("Class %s requires completion of class %s" % (this, req))
+
