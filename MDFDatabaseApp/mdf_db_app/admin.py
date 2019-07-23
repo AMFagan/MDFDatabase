39cf04c725ac0a19ed17a3fe78ee990e9d9c0c91
AndyF1996 <andrewfagan1996@gmail.com>
2019-07-23 11:17:17 +0100

    Rebuilt project to use Django web app frontend

---
+++ MDFDatabaseApp/mdf_db_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from mdf_db_app.models import *
+
+# Register your models here.
+
+admin.site.register(Module)
+admin.site.register(Coursework)
+admin.site.register(Exam)
+admin.site.register(Staff)
+admin.site.register(Assignment)
