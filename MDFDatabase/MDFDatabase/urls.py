f9650a3cb75e8ae8a1efa06d13507bff7e695eae
AMFagan <andrew.fagan@strath.ac.uk>
2020-03-10 10:32:09 +0000

    Added index page

--- MDFDatabase/MDFDatabase/urls.py
+++ MDFDatabase/MDFDatabase/urls.py
@@ -4,14 +4,16 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-
+    path('', views.home),
     path('admin/', admin.site.urls),
-    path('modules/', views.module_index),
+    path('modules/', views.mdf_index),
     url(r'modules/(?P<code>[A-Za-z0-9]{5})', views.mdf_test),
     path('staff/', views.staff_index),
     url(r'staff/(?P<id_number>\d+)', views.staff_member),
     path('courses/', views.course_index),
     url(r'courses/(?P<level>[A-Za-z]+)_(?P<shorthand>[A-Za-z]+)', views.course),
+    path('predecessors/', views.pre_index),
     url(r'predecessors/(?P<code>[A-Za-z0-9]{5})', views.dependancies),
+    path('successors/', views.succ_index),
     url(r'successors/(?P<code>[A-Za-z0-9]{5})', views.successors),
 ]
--- MDFDatabase/MDFDatabase/views.py
+++ MDFDatabase/MDFDatabase/views.py
@@ -6,24 +6,37 @@ from MDFDatabase.models import Module, Course, Staff
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-#import weasyprint as weas
+# import weasyprint as weas
 import tempfile
 
 
 def mdf_test(request, code):
     module = get_object_or_404(Module, code=code)
-    #html = weas.HTML(string=render_to_string('mdf.xhtml', {'module': module}))
-    #result = html.write_pdf()
+    # html = weas.HTML(string=render_to_string('mdf.xhtml', {'module': module}))
+    # result = html.write_pdf()
 
     # Creating http response
-    response = HttpResponse(render_to_string('mdf.xhtml', {'module': module})) #result, content_type='application/pdf;')
-
+    response = HttpResponse(
+        render_to_string('mdf.xhtml', {'module': module}))  # result, content_type='application/pdf;')
 
     return response
 
-def module_index(request):
+
+def module_index(title, request):
     entries = Module.objects.order_by('level', 'code')
-    return HttpResponse(render_to_string('module_index.xhtml', {'entries': entries}))
+    return HttpResponse(render_to_string('module_index.xhtml', {'title': title, 'entries': entries}))
+
+
+def mdf_index(request):
+    return module_index("MDF Index", request)
+
+
+def pre_index(request):
+    return module_index("Pre Requisite Index", request)
+
+
+def succ_index(request):
+    return module_index("Successor Index", request)
 
 
 def staff_index(request):
@@ -41,16 +54,23 @@ def course_index(request):
     entries = Course.objects.all()
     return HttpResponse(render_to_string('course_index.xhtml', {'entries': entries}))
 
+
 def course(request, level, shorthand):
     c = get_object_or_404(Course, award=level, shorthand=shorthand)
     return HttpResponse(render_to_string('course_page.xhtml', {'course': c, 'levels': c.levels()}))
+
 
 def dependancies(request, code):
     module = get_object_or_404(Module, code=code)
     response = HttpResponse(render_to_string('dependancy.xhtml', {'module': module, 'list': module.as_tree_pres()}))
     return response
 
+
 def successors(request, code):
     module = get_object_or_404(Module, code=code)
     response = HttpResponse(render_to_string('dependancy.xhtml', {'module': module, 'list': module.as_tree_succs()}))
     return response
+
+
+def home(request):
+    return HttpResponse(render_to_string('home.xhtml', {}))
